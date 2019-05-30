f71a04d683ddfa344f75bf6160cafdbb04ab903d
FabsandFabs <sfefromsfe@gmail.com>
2019-05-30 20:00:05 +0200

    Create db.js

---
+++ db.js
@@ -0,0 +1,100 @@
+{
+	"services": [
+		{
+		    "name":"copywrite",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit seddo eiusmodtempor incididunt ut labore et dolore magna aliqua.",
+		    "download":"https://www.acantocomunicazione.it/newsite/assets/placeholder/atlasplan.png",
+        "projects":[    
+		        {
+              "project":"full services",
+              "client":"atlas plan",
+              "images":"https://www.acantocomunicazione.it/newsite/assets/placeholder/atlasplan.png"
+            },
+		        {
+              "project":"ghost",
+              "client":"fiam",
+              "images":"https://www.acantocomunicazione.it/newsite/assets/placeholder/hsd.png"
+            },
+		        {
+              "project":"Journey",
+              "client":"TeamSystem",
+              "images":"https://www.acantocomunicazione.it/newsite/assets/placeholder/atlasplan.png"
+            }, 
+		        {
+              "project":"catalogue",
+              "client":"atlas concorde",
+              "images":"https://www.acantocomunicazione.it/newsite/assets/placeholder/hsd.png"
+            },
+		        {
+              "project":"Journey",
+              "client":"TeamSystem",
+              "images":"https://www.acantocomunicazione.it/newsite/assets/placeholder/atlasplan.png"
+            }             
+		    ]
+		},
+		{
+		    "name":"Social Strategy",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit seddo eiusmodtempor incididunt ut labore et dolore magna aliqua.",
+		    "download":"",
+        "projects":[    
+		        {
+              "project":"full services",
+              "client":"atlas plan",
+              "images":"https://www.acantocomunicazione.it/newsite/assets/placeholder/atlasplan.png"
+            },
+		        {
+              "project":"ghost",
+              "client":"fiam",
+              "images":"https://www.acantocomunicazione.it/newsite/assets/placeholder/hsd.png"
+            },
+		        {
+              "project":"Journey",
+              "client":"TeamSystem",
+              "images":"https://www.acantocomunicazione.it/newsite/assets/placeholder/atlasplan.png"
+            }, 
+		        {
+              "project":"catalogue",
+              "client":"atlas concorde",
+              "images":"https://www.acantocomunicazione.it/newsite/assets/placeholder/hsd.png"
+            },
+		        {
+              "project":"Journey",
+              "client":"TeamSystem",
+              "images":"https://www.acantocomunicazione.it/newsite/assets/placeholder/atlasplan.png"
+            }             
+		    ]
+		},
+		{
+		    "name":"Analytics",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit seddo eiusmodtempor incididunt ut labore et dolore magna aliqua.",
+		    "download":"",
+        "projects":[    
+		        {
+              "project":"full services",
+              "client":"atlas plan",
+              "images":"https://www.acantocomunicazione.it/newsite/assets/placeholder/atlasplan.png"
+            },
+		        {
+              "project":"ghost",
+              "client":"fiam",
+              "images":"https://www.acantocomunicazione.it/newsite/assets/placeholder/hsd.png"
+            },
+		        {
+              "project":"Journey",
+              "client":"TeamSystem",
+              "images":"https://www.acantocomunicazione.it/newsite/assets/placeholder/atlasplan.png"
+            }, 
+		        {
+              "project":"catalogue",
+              "client":"atlas concorde",
+              "images":"https://www.acantocomunicazione.it/newsite/assets/placeholder/hsd.png"
+            },
+		        {
+              "project":"Journey",
+              "client":"TeamSystem",
+              "images":"https://www.acantocomunicazione.it/newsite/assets/placeholder/atlasplan.png"
+            }             
+		    ]
+		},    
+	]
+}
